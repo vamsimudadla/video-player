52224ee448a6d40ca314232f8ad2d20b5d8e06b2
vamsimudadla <mudadlavamsi@gmail.com>
2019-12-18 13:41:13 +0530

    removed unnecessary things

--- src/Components/Video/index.tsx
+++ src/Components/Video/index.tsx
@@ -3,45 +3,14 @@ import * as React from 'react';
 import videoJS from 'video.js';
 import 'video.js/dist/video-js.min.css';
 
-import { StyledVideo, VideoContainer, VideoOverlay } from './styledComponents';
+import { VideoContainer } from './styledComponents';
 import videoConfig from './config';
 
 import './video-overridden.css';
 import './vjs-markers.css';
 
-interface VideoProps {
-  width: number;
-  height: number;
-  id: string;
-  testId: string;
-  videoClassName: string;
-  videoContainerClassName: string;
-}
-
-interface VideoState {
-  isPlaying: boolean;
-}
-
-class Video extends React.Component<VideoProps, VideoState> {
-  // TODO: annotate type for videoPlayer
+class Video extends React.Component {
   videoPlayer: any;
-
-  static defaultProps = {
-    width: 640,
-    height: 360,
-    id: 'video-player',
-    testId: 'test-video-player',
-    videoClassName: '',
-    videoContainerClassName: ''
-  };
-
-  constructor(props: VideoProps) {
-    super(props);
-
-    this.state = {
-      isPlaying: false
-    };
-  }
 
   componentDidMount() {
     this.setupVideoPlayer();
@@ -53,101 +22,24 @@ class Video extends React.Component<VideoProps, VideoState> {
     }
   }
 
-  playVideo = () => {
-    try {
-      const { isPlaying } = this.state;
-      if (!isPlaying) {
-        if (this.videoPlayer) {
-          this.videoPlayer.play();
-        }
-      }
-    } catch (e) {}
-  };
-
-  changeCurrentTime = (time: number) => {
-    if (this.videoPlayer) {
-      this.videoPlayer.currentTime(time);
-    }
-  };
-
-  changeCurrentTimeAndPlay = (time: number) => {
-    this.changeCurrentTime(time);
-    this.playVideo();
-  };
-
   setupVideoPlayer = () => {
     const options = videoConfig;
-    const { id } = this.props;
 
-    this.videoPlayer = videoJS(id, options, () => {
+    this.videoPlayer = videoJS('video', options, () => {
       videoJS.log('Your player is ready!');
-      this.registerEvents();
     });
 
-    this.customizeControlBar();
-    this.changeCurrentTimeAndPlay(60);
-  };
-
-  registerEvents = () => {
-    this.videoPlayer.on('play', this.onPlay);
-    this.videoPlayer.on('pause', this.onPause);
-    this.videoPlayer.on('ended', this.onEnd);
-  };
-
-  customizeControlBar = () => {
-    const spacer = document.createElement('div');
-    spacer.className = 'vjs-custom-control-spacer vjs-spacer';
-
-    const controlBar = document.getElementsByClassName('vjs-control-bar')[0];
-    const playbackRateEl = document.getElementsByClassName(
-      'vjs-playback-rate'
-    )[0];
-    controlBar.insertBefore(spacer, playbackRateEl);
-  };
-
-  getCurrentTimeInSec = () => {
-    return this.videoPlayer.currentTime().toFixed(0);
-  };
-
-  onPlay = () => {
-    this.setState({
-      isPlaying: true
-    });
-  };
-
-  getVideoDurationInSec = () => {
-    return this.videoPlayer.duration();
-  };
-
-  onPause = () => {
-    this.setState({
-      isPlaying: false
-    });
-  };
-
-  onEnd = () => {
-    this.setState({
-      isPlaying: false
-    });
+    this.videoPlayer.currentTime(60);
   };
 
   render() {
-    const {
-      width,
-      height,
-      id,
-      testId,
-      videoClassName,
-      videoContainerClassName
-    } = this.props;
-    const { isPlaying } = this.state;
     return (
-      <VideoContainer data-testid={testId} className={videoContainerClassName}>
-        <StyledVideo
-          className={`video-js vjs-default-skin vjs-big-play-centered ${videoClassName}`}
+      <VideoContainer>
+        <video
+          className={`video-js vjs-default-skin vjs-big-play-centered`}
           controls
           preload='auto'
-          id={id}
+          id='video'
           poster='https://html5box.com/html5gallery/images/Big_Buck_Bunny_1_1024.jpg'
           width={640}
           height={360}
@@ -157,14 +49,7 @@ class Video extends React.Component<VideoProps, VideoState> {
             src='http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8'
             type='application/x-mpegURL'
           />
-        </StyledVideo>
-        {!isPlaying && (
-          <VideoOverlay
-            onClick={() => {
-              this.playVideo();
-            }}
-          />
-        )}
+        </video>
       </VideoContainer>
     );
   }
